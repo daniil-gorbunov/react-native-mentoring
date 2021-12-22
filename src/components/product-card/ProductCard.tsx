74eae79c2dc8b6545fd794d23a12ae8b8b8950a4
Daniil Gorbunov <17042755+daniil-gorbunov@users.noreply.github.com>
2021-12-21 19:18:37 -0800

    Product price line moved to distinct component

--- src/components/product-card/ProductCard.tsx
+++ src/components/product-card/ProductCard.tsx
@@ -1,15 +1,11 @@
 import React from 'react';
 import {Image, StyleSheet, Text, View} from 'react-native';
 import {Product} from 'services/products';
+import {PriceLine} from 'components';
 
 interface ProductCardProps {
   product: Product;
 }
-
-const getDiscountLine = (newPrice: number, oldPrice?: number): string =>
-  oldPrice && oldPrice > newPrice
-    ? `${Math.round(100 * (1 - newPrice / oldPrice))}% Off`
-    : '';
 
 export const ProductCard: React.FC<ProductCardProps> = ({
   product: {name, image, price, prevPrice},
@@ -19,13 +15,7 @@ export const ProductCard: React.FC<ProductCardProps> = ({
       <Image style={styles.image} source={image} />
       <View style={styles.subtext}>
         <Text style={styles.name}>{name}</Text>
-        <View style={styles.prices}>
-          <Text style={styles.price}>${price}</Text>
-          <Text style={styles.prevPrice}>${prevPrice}</Text>
-          <Text style={styles.discount}>
-            {getDiscountLine(price, prevPrice)}
-          </Text>
-        </View>
+        <PriceLine price={price} prevPrice={prevPrice} />
       </View>
     </View>
   );
@@ -55,29 +45,5 @@ const styles = StyleSheet.create({
     fontFamily: 'Roboto',
     fontSize: 15,
     fontWeight: '400',
-  },
-  prices: {
-    width: '100%',
-    justifyContent: 'space-between',
-    flexDirection: 'row',
-  },
-  price: {
-    color: '#4a4a4a',
-    fontFamily: 'Roboto',
-    fontWeight: '700',
-    flexGrow: 1,
-  },
-  prevPrice: {
-    fontFamily: 'Roboto',
-    fontWeight: '700',
-    color: '#8f8f8f',
-    textDecorationLine: 'line-through',
-    flexGrow: 1,
-  },
-  discount: {
-    fontFamily: 'Roboto',
-    fontWeight: '700',
-    color: '#00a8f3',
-    flexGrow: 1,
   },
 });
