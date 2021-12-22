70c1d73de1c5752610db892a0f2f75a3cb838f0b
Daniil Gorbunov <17042755+daniil-gorbunov@users.noreply.github.com>
2021-12-21 19:13:35 -0800

    Use props in AppBar component

--- src/components/app-bar/AppBar.tsx
+++ src/components/app-bar/AppBar.tsx
@@ -2,12 +2,43 @@ import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
-export const AppBar: React.FC = () => {
+interface AppBarProps {
+  title?: string;
+  showMenuButton?: boolean;
+  showFavButton?: boolean;
+  onFavPress?: () => void;
+}
+
+export const AppBar: React.FC<AppBarProps> = ({
+  title = '',
+  showMenuButton = false,
+  showFavButton = false,
+  onFavPress,
+}) => {
   return (
     <View style={styles.barContainer}>
-      <Icon name={'menu'} size={25} color="#fff" />
-      <Text style={styles.title}>Ecommerce Store</Text>
-      <Icon name={'shopping-cart'} size={25} color="#fff" />
+      {showMenuButton ? (
+        <Icon name={'menu'} size={25} color="#fff" />
+      ) : (
+        <Icon name={'arrow-back'} size={25} color="#fff" />
+      )}
+      <Text style={styles.title}>{title}</Text>
+      <View style={styles.icons}>
+        {showFavButton && (
+          <Icon
+            onPress={onFavPress}
+            name={'favorite-border'}
+            size={25}
+            color="#fff"
+          />
+        )}
+        <Icon
+          style={styles.cartIcon}
+          name={'shopping-cart'}
+          size={25}
+          color="#fff"
+        />
+      </View>
     </View>
   );
 };
@@ -24,5 +55,13 @@ const styles = StyleSheet.create({
     fontSize: 20,
     fontWeight: '500',
     fontFamily: 'Roboto',
+  },
+  icons: {
+    width: 70,
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+  },
+  cartIcon: {
+    paddingLeft: 25,
   },
 });
